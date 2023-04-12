91ab36559ad364e443a2be7496069321cf1e616f
Olga Polishchuk <olgapolichuk@ukr.net>
2023-04-12 16:43:52 +0300

    add special first element conditions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './js/headerMenu.js';
 import './js/headerScroll.js';
 import './js/reviews.js';
-import './js/addToBasket.js';
 import './js/allOffers.js';
+import './js/addToBasket.js';
 import './js/scroll-to-top.js';
 import './js/headerBasketChange.js';
 
--- src/js/addToBasket.js
+++ src/js/addToBasket.js
@@ -4,10 +4,17 @@ import { refs } from './refs';
 const LS_KEY_ADD_TO = 'Add-to-basket';
 
 const body = document.querySelector('body');
+const specialReduceBtn = document.querySelector('.js-reduce-special');
+const specialAddeBtn = document.querySelector('.js-add-special');
+const specialOrdereBtn = document.querySelector('.js-order-special');
 
 body.addEventListener('click', handleAddValueBtnClick);
 body.addEventListener('click', handleReduceValueBtnClick);
 body.addEventListener('click', handleOrderBtnClick);
+specialReduceBtn.addEventListener('click', handleReduceSpecialBtnClick);
+specialAddeBtn.addEventListener('click', handleAddSpecialBtnClick);
+specialOrdereBtn.addEventListener('click', handleOrderSpecialBtnClick);
+
 // checkLS(LS_KEY_ADD_TO);
 if (refs.specials)
   refs.specials.addEventListener('click', handleSpecialBtnClick);
@@ -55,7 +62,8 @@ function handleOrderBtnClick(e) {
   orderBtn.textContent = "В кошику";
   addBtn.setAttribute('disabled', 'disabled');
   reduceBtn.setAttribute('disabled', 'disabled');
-  refs.basketBtn.classList.add('header-btn-basket-full');
+  const basketBtnFull = document.querySelector('.header-btn-basket-full');
+  if (!basketBtnFull) refs.basketBtn.classList.add('header-btn-basket-full');
 }
 
 function handleSpecialBtnClick() {
@@ -74,4 +82,58 @@ function handleSpecialBtnClick() {
 function checkLSById(id) {
   let data = getDataFromLockalStorageByKey(LS_KEY_ADD_TO);
   return data.find(article => article.id === id);
+}
+
+function handleAddValueBtnClick(e) {
+  const elem = e.target;
+  const addBtn = elem.closest('.js-add');
+  if (elem !== addBtn) return;
+  let valueOutput = elem.previousElementSibling;
+  let value = Number(valueOutput.textContent);
+  value += 1;
+  valueOutput.textContent = value;
+  return;
+}
+
+function handleReduceSpecialBtnClick(e) {
+  const elem = e.target;
+  const orderArticleElem = elem.closest('.js-articleId');
+
+  let valueOutput = orderArticleElem.querySelector('.js-value');
+  let value = Number(valueOutput.textContent);
+  value = value <= 0 ? 0 : value - 5;
+  valueOutput.textContent = value;
+  return;
+}
+
+function handleAddSpecialBtnClick(e) {
+  const elem = e.target;
+  const orderArticleElem = elem.closest('.js-articleId');
+
+  let valueOutput = orderArticleElem.querySelector('.js-value');
+  let value = Number(valueOutput.textContent);
+  value += 5;
+  valueOutput.textContent = value;
+  return;
+}
+
+function handleOrderSpecialBtnClick(e) {
+  const elem = e.target;
+  const orderArticleIdelem = elem.closest('.js-articleId');
+  const id = orderArticleIdelem.id;
+  const valueElem = orderArticleIdelem.querySelector('.js-value');
+  const value = Number(valueElem.textContent);
+  if (value === 0) return;
+  const dataByLs = checkLS(LS_KEY_ADD_TO);
+  const check = checkLSById(id);
+  if (check) {
+    alert('Ця нитка вже додана. Перейдіть в кошик для завершення замовлення.');
+    return;
+  }
+  setDataToLocalStorageByKey(LS_KEY_ADD_TO, [...dataByLs, ...[{ id, value }]]);
+  specialOrdereBtn.textContent = "В кошику";
+  specialAddeBtn.setAttribute('disabled', 'disabled');
+  specialReduceBtn.setAttribute('disabled', 'disabled');
+  const basketBtnFull = document.querySelector('.header-btn-basket-full');
+  if (!basketBtnFull) refs.basketBtn.classList.add('header-btn-basket-full');
 }
