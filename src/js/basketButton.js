304e760341bce7010a94ef9c2918fc7f876097df
Anna Krysenko <krysenkoanna@gmail.com>
2023-04-16 02:53:44 +0300

    fix btn in basket

--- src/js/basketButton.js
+++ src/js/basketButton.js
@@ -1,7 +1,8 @@
 import { refs } from './refs';
 
 if (refs.goToOffersBtn) {
-  refs.goToOffersBtn.addEventListener('click', () => {
+  refs.goToOffersBtn.addEventListener('click', e => {
+    console.log(e.target.dataset.section);
     localStorage.setItem('scroll', e.target.dataset.section);
     document.location.pathname = '/chervona-nytka/index.html';
   });
