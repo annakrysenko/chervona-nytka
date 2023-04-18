8ac5d4a06d9ae846e7962dde8f05c39d4e955e70
Anna Krysenko <krysenkoanna@gmail.com>
2023-04-18 12:45:28 +0300

    fix

--- src/js/basketButton.js
+++ src/js/basketButton.js
@@ -1,7 +1,7 @@
 import { refs } from './refs';
 
 refs.goToOffersBtn.addEventListener('click', e => {
-  console.log(first);
+  console.log('first');
   localStorage.setItem('scroll', e.target.dataset.section);
   document.location.pathname = '/chervona-nytka/index.html';
 });
